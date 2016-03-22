86c3d1d858985234a64a1d780c23c257e5cf141e
Chris LaRose <cjlarose@gmail.com>
2016-03-21 20:53:11 -0700

    Move raw library into separate module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-var ffi = require('ffi');
-var ref = require('ref');
-
-var argon2 = ffi.Library(__dirname + '/../build/Release/argon2', {
-  argon2i_hash_raw: ['int', ['uint32', /* t_cost */
-                             'uint32', /* m_cost */
-                             'uint32', /* parallelism */
-                             ref.refType('void'), /* password */
-                             'size_t', /* password length */
-                             ref.refType('void'), /* salt */
-                             'size_t', /* salt length */
-                             ref.refType('void'), /* hash */
-                             'size_t']] /* hash length */
-});
+var argon2 = require('./library');
 
 var timeCost = 3;
 var memoryCost = 12; // 2^12 KiB
---
+++ src/library.js
@@ -0,0 +1,16 @@
+var ffi = require('ffi');
+var ref = require('ref');
+
+var argon2 = ffi.Library(__dirname + '/../build/Release/argon2', {
+  argon2i_hash_raw: ['int', ['uint32', /* t_cost */
+                             'uint32', /* m_cost */
+                             'uint32', /* parallelism */
+                             ref.refType('void'), /* password */
+                             'size_t', /* password length */
+                             ref.refType('void'), /* salt */
+                             'size_t', /* salt length */
+                             ref.refType('void'), /* hash */
+                             'size_t']] /* hash length */
+});
+
+module.exports = argon2;
