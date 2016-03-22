9a4825608dad287c439b1d47d1c4c48a59f9f023
Chris LaRose <chris.larose@mycase.com>
2016-03-21 20:49:58 -0700

    Working example

--- src/index.js
+++ src/index.js
@@ -13,4 +13,16 @@ var argon2 = ffi.Library(__dirname + '/../build/Release/argon2', {
                              'size_t']] /* hash length */
 });
 
-var result = argon2.argon2i_hash_raw();
+var timeCost = 3;
+var memoryCost = 12; // 2^12 KiB
+var parallelism = 1;
+
+var password = new Buffer("password1");
+var salt = password;
+
+var hashOutput = new Buffer(32);
+var hashLength = 32;
+
+var result = argon2.argon2i_hash_raw(timeCost, memoryCost, parallelism, password, password.length, salt, salt.length, hashOutput, hashLength);
+console.log(result);
+console.log(hashOutput.toString('hex'));
