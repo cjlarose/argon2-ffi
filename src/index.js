592d7bc95e85301156d4a0a9bc0d7ca3821649b9
Chris LaRose <chris.larose@mycase.com>
2016-03-21 20:35:30 -0700

    Fix types

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 var ffi = require('ffi');
 var ref = require('ref');
 
-/*
-var libm = ffi.Library('libm', {
-    'ceil': [ 'double', [ 'double' ] ]
-});
-libm.ceil(1.5); // 2
-console.log(libm.ceil(1.5));
-*/
-
 var argon2 = ffi.Library(__dirname + '/../build/Release/argon2', {
-  argon2i_hash_raw: ['int', ['uint32_t', /* t_cost */
-                             'uint32_t', /* m_cost */
-                             'uint32_t', /* parallelism */
+  argon2i_hash_raw: ['int', ['uint32', /* t_cost */
+                             'uint32', /* m_cost */
+                             'uint32', /* parallelism */
                              ref.refType('void'), /* password */
                              'size_t', /* password length */
                              ref.refType('void'), /* salt */
@@ -22,9 +14,3 @@ var argon2 = ffi.Library(__dirname + '/../build/Release/argon2', {
 });
 
 var result = argon2.argon2i_hash_raw();
-
-// You can also access just functions in the current process by passing a null
-// var current = ffi.Library(null, {
-//   'atoi': [ 'int', [ 'string' ] ]
-//   });
-//   current.atoi('1234'); // 1234
