1f48d641af1b9904f9fe63a3132addf507cf771a
Lee Bousfield <ljbousfield@gmail.com>
2016-04-07 09:44:02 -0600

    Switch to Promises
    Resolves #2, resolves #3

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ password storage by the [Open Web Application Security Project
 password hashing and validation are performed asynchronously by dispatching the
 work to a separate thread pool using [`node-ffi`, which in turn uses
 `libuv`][async-library-calls], so your main application can continue to do
-other work while these tasks are executed.
+other work while these tasks are executed. All asynchronous operations return
+Promises, with a type defined by [any-promise](https://www.npmjs.com/package/any-promise).
 
 [async-library-calls]: https://github.com/node-ffi/node-ffi/wiki/Node-FFI-Tutorial#async-library-calls
 
@@ -29,13 +30,19 @@ one you should use, refer to the [`argon2` repo][argon2].
 ```javascript
 var argon2i = require('argon2-ffi').argon2i;
 // var argon2d = require('argon2-ffi').argon2d; if you'd like to use argon2d
+var crypto = require('crypto');
+var Promise = require('bluebird');
+var randomBytes = Promise.promisify(crypto.randomBytes);
 
-var password = new Buffer('password1');
-var salt = new Buffer('saltysalt');
-argon2i.hash(password, salt, function(err, res) {
-  console.log(res); // $argon2i$v=19$m=4096,t=3,p=1$c2FsdHlzYWx0$oG0js25z7kM30xSg9+nAKtU0hrPa0UnvRnqQRZXHCV8
-});
+var password = 'password1'; // Can also be a Buffer
+crypto.randomBytes(32).then(salt => argon2i.hash(password, salt))
+  .then(console.log); // $argon2i$v=19$m=4096,t=3,p=1$c2FsdHlzYWx0$oG0js25z7kM30xSg9+nAKtU0hrPa0UnvRnqQRZXHCV8
 ```
+
+In this example,
+[crypto.randomBytes](https://nodejs.org/api/crypto.html#crypto_crypto_randombytes_size_callback)
+is used to generate a salt. This is the best practice as the salt is guaranteed
+to be cryptographically secure. However, you can of course use your own buffer.
 
 `.hash` takes a few options, too! You can specify `timeCost` (default `3`),
 `memoryCost` (default `4096`), `parallelism` (default
@@ -44,13 +51,14 @@ have an effect on the output hash.
 
 ```javascript
 var argon2i = require('argon2-ffi').argon2i;
+var crypto = require('crypto');
+var Promise = require('bluebird');
+var randomBytes = Promise.promisify(crypto.randomBytes);
 
 var password = new Buffer('password1');
-var salt = new Buffer('saltysalt');
 var options = { timeCost: 4, memoryCost: 1 << 14, parallelism: 2, hashLength: 64 };
-argon2i.hash(password, salt, options, function(err, res) {
-  console.log(res); // $argon2i$v=19$m=16384,t=4,p=2$c2FsdHlzYWx0$gwJY/FsXNSR3aS1ChVTgDZ9HbF3V7sbbYE5UmQsdXFHB4Tt6/RVtFWGIIJnzZ62nL9miurrvJnxhvORK64ddFg
-});
+crypto.randomBytes(32).then(salt => argon2i.hash(password, salt, options))
+  .then(console.log); // $argon2i$v=19$m=16384,t=4,p=2$c2FsdHlzYWx0$gwJY/FsXNSR3aS1ChVTgDZ9HbF3V7sbbYE5UmQsdXFHB4Tt6/RVtFWGIIJnzZ62nL9miurrvJnxhvORK64ddFg
 ```
 
 The result of running `.hash` is a string that encodes all of the options used
@@ -59,19 +67,13 @@ as we'll see in the next section.
 
 ### Verifying a password
 
-
 ```javascript
 var argon2i = require('argon2-ffi').argon2i;
 
 var encodedHash = "$argon2i$v=19$m=4096,t=3,p=1$c2FsdHlzYWx0$oG0js25z7kM30xSg9+nAKtU0hrPa0UnvRnqQRZXHCV8";
 var password = new Buffer('password1');
-argon2i.verify(encodedHash, password, function(err, res) {
-  if (err) {
-    console.error('Incorrect password');
-    return;
-  }
-  console.log('Correct password!');
-});
+argon2i.verify(encodedHash, password)
+  .then(correct => console.log(correct ? 'Correct password!' : 'Incorrect password'));
 ```
 
 ## Contributing
--- test/argon2.js
+++ test/argon2.js
@@ -6,169 +6,134 @@ var argon2d = require('../lib/index').argon2d;
 
 describe('argon2i', function () {
   describe('hashRaw', function () {
-    it('should return correct hash', function (done) {
+    it('should return correct hash', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var expectedHash = '5179f7706253f090137b74004f16355341405aa657ac3ed1e9b5301326778444';
-      argon2i.hashRaw(password, salt, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2i.hashRaw(password, salt)
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
 
-    it('should allow changing timeCost', function (done) {
+    it('should allow changing timeCost', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var expectedHash = '188baa8a1acbad59af6d3acfaa8a63b6845e64ac4d95c7be8b29ea9c00e2edcb';
-      argon2i.hashRaw(password, salt, { timeCost: 5 }, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2i.hashRaw(password, salt, { timeCost: 5 })
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
 
-    it('should allow changing memoryCost', function (done) {
+    it('should allow changing memoryCost', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var expectedHash = 'f7b4b5b0c30976cdf3137281a31a0573e0335723882388320069b58b94793fb7';
-      argon2i.hashRaw(password, salt, { memoryCost: 1 << 14 }, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2i.hashRaw(password, salt, { memoryCost: 1 << 14 })
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
 
-    it('should allow changing parallelism', function (done) {
+    it('should allow changing parallelism', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var expectedHash = '804a7d9ed278d4125e5bfb710972ad105235227215a3f9e7c2cda304f7d4b81d';
       var options = { memoryCost: 1 << 16, parallelism: 2 };
-      argon2i.hashRaw(password, salt, options, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2i.hashRaw(password, salt, options)
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
 
-    it('should return error codes', function (done) {
+    it('should return error codes', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var options = { timeCost: 2, memoryCost: 1, parallelism: 1 };
-      argon2i.hashRaw(password, salt, options, function (err, hashOutput) {
-        assert(err instanceof Error);
-        assert.equal(hashOutput, null);
-        assert.equal(err.message, 'Memory cost is too small');
-        done();
-      });
+      return argon2i.hashRaw(password, salt, options)
+        .catch(err => err)
+        .then(function (err) {
+          assert(err instanceof Error);
+          assert.equal(err.message, 'Memory cost is too small');
+        });
     });
 
-    it('should allow password input of type string', function (done) {
+    it('should allow password input of type string', function () {
       var salt = new Buffer('saltsalt');
       var password = 'password1';
       var expectedHash = '5179f7706253f090137b74004f16355341405aa657ac3ed1e9b5301326778444';
-      argon2i.hashRaw(password, salt, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2i.hashRaw(password, salt)
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
   });
 
   describe('hash', function () {
-    it('should return encoded hash', function (done) {
+    it('should return encoded hash', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var expectedEncoding = '$argon2i$v=19$m=4096,t=3,p=1' +
         '$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ';
-      argon2i.hash(password, salt, function (err, res) {
-        assert.ifError(err);
-        assert.equal(res, expectedEncoding);
-        done();
-      });
+      return argon2i.hash(password, salt)
+        .then(hash => assert.equal(hash.toString('hex'), expectedEncoding));
     });
 
-    it('should allow password input of type string', function (done) {
+    it('should allow password input of type string', function () {
       var salt = new Buffer('saltsalt');
       var password = 'password1';
       var expectedHash = '$argon2i$v=19$m=4096,t=3,p=1' +
         '$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ';
-      argon2i.hash(password, salt, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2i.hash(password, salt)
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
   });
 
   describe('verify', function () {
-    it('should verify password', function (done) {
+    it('should verify password', function () {
       var password = new Buffer('password1');
       var encodedHash = '$argon2i$v=19$m=4096,t=3,p=1' +
         '$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ';
-      argon2i.verify(encodedHash, password, function (err) {
-        assert.ifError(err);
-        done();
-      });
+      return argon2i.verify(encodedHash, password)
+        .then(correct => assert(correct));
     });
 
-    it('should allow password input of type string', function (done) {
+    it('should allow password input of type string', function () {
       var password = 'password1';
       var encodedHash = '$argon2i$v=19$m=4096,t=3,p=1' +
         '$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ';
-      argon2i.verify(encodedHash, password, function (err) {
-        assert.ifError(err);
-        done();
-      });
+      return argon2i.verify(encodedHash, password)
+        .then(correct => assert(correct));
     });
 
-    it('should reject bad password', function (done) {
+    it('should resolve false for a bad password', function () {
       var password = new Buffer('password10');
       var encodedHash = '$argon2i$v=19$m=4096,t=3,p=1' +
         '$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ';
-      argon2i.verify(encodedHash, password, function (err, res) {
-        assert(err instanceof Error);
-        assert.equal(res, null);
-        assert.equal(err.message, 'The password does not match the supplied hash');
-        done();
-      });
+      return argon2i.verify(encodedHash, password)
+        .then(correct => assert(!correct));
     });
 
-    it('should reject undefined hash', function (done) {
+    it('should reject undefined hash', function () {
       var password = new Buffer('password10');
 
-      argon2i.verify(undefined, password, function (err, res) {
-        assert(err instanceof Error);
-        assert.equal(res, null);
-        assert.equal(err.message, 'Decoding failed');
-        done();
-      });
+      return argon2i.verify(undefined, password)
+        .catch(err => err)
+        .then(err => {
+          assert(err instanceof Error);
+          assert.equal(err.message, 'Decoding failed');
+        });
     });
   });
 });
 
 describe('argon2d', function () {
   describe('hashRaw', function () {
-    it('should return correct hash', function (done) {
+    it('should return correct hash', function () {
       var salt = new Buffer('saltsalt');
       var password = new Buffer('password1');
       var expectedHash = '12d8487548a17c7856a26abf640fcdfd5ab0e4f57188292d2ef634299f43fc2f';
-      argon2d.hashRaw(password, salt, function (err, hashOutput) {
-        assert.ifError(err);
-        assert.equal(hashOutput.toString('hex'), expectedHash);
-        done();
-      });
+      return argon2d.hashRaw(password, salt)
+        .then(hash => assert.equal(hash.toString('hex'), expectedHash));
     });
   });
 
-  it('should allow password input of type string', function (done) {
+  it('should allow password input of type string', function () {
     var salt = new Buffer('saltsalt');
     var password = 'password1';
     var expectedHash = '12d8487548a17c7856a26abf640fcdfd5ab0e4f57188292d2ef634299f43fc2f';
-    argon2d.hashRaw(password, salt, function (err, hashOutput) {
-      assert.ifError(err);
-      assert.equal(hashOutput.toString('hex'), expectedHash);
-      done();
-    });
+    return argon2d.hashRaw(password, salt)
+      .then(hash => assert.equal(hash.toString('hex'), expectedHash));
   });
 });
