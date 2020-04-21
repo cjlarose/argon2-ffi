e686363175d286bfdabec229eaae0c9f2ff8fd1d
Chris LaRose <cjlarose@gmail.com>
2020-04-21 08:29:13 -0700

    Format types test with prettier

--- types/test.ts
+++ types/test.ts
@@ -1,11 +1,11 @@
-import { argon2i, argon2d } from 'argon2-ffi';
+import { argon2i, argon2d } from "argon2-ffi";
 
-const salt = new Buffer('saltsalt');
-const password = new Buffer('password1');
+const salt = new Buffer("saltsalt");
+const password = new Buffer("password1");
 const options = { memoryCost: 1 << 16, parallelism: 2 };
 const encodedHash =
-  '$argon2i$v=19$m=4096,t=3,p=1' +
-  '$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ';
+  "$argon2i$v=19$m=4096,t=3,p=1" +
+  "$c2FsdHNhbHQ$UXn3cGJT8JATe3QATxY1U0FAWqZXrD7R6bUwEyZ3hEQ";
 argon2i.hash(password, salt);
 argon2i.hashRaw(password, salt, { timeCost: 5 });
 argon2i.hashRaw(password, salt, { memoryCost: 1 << 14 });
